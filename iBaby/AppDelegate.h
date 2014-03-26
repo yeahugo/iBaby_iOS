3e89923bcfcdbf8ea06f4c964288d26e1acbffef
yeahugo <yejiahuan@umeng.com>
2014-03-26 08:04:58 +0800

    Initial Commit

---
+++ iBaby/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBaby
+//
+//  Created by yeahugo on 14-3-26.
+//  Copyright (c) 2014年 Ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iBaby/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBaby
+//
+//  Created by yeahugo on 14-3-26.
+//  Copyright (c) 2014年 Ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
